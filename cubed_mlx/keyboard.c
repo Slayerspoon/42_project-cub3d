0a97ebde7be4f8fc96d751882691af3ff8bc5104
LidiaOrfanu <lidia3991@yahoo.com>
2022-10-31 12:36:57 +0100

    testing

--- cubed_mlx/keyboard.c
+++ cubed_mlx/keyboard.c
@@ -6,7 +6,7 @@
 /*   By: lorfanu <lorfanu@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 16:56:25 by lorfanu           #+#    #+#             */
-/*   Updated: 2022/10/27 18:44:14 by lorfanu          ###   ########.fr       */
+/*   Updated: 2022/10/31 12:32:02 by lorfanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,23 @@ int	game_xbutton(t_gamedata *game)
 int	handle_game_keys(int key, t_gamedata *game)
 {
 	// if (key == 'w' || key == 'W')
-	// 	player_moves_forward(game);
+	// 	move_forward_w(game);
 	// else if (key == 's' || key == 'S')
-	// 	player_moves_backward(game);
+	// 	move_backward_s(game);
 	// else if (key == 'd' || key == 'D')
-	// 	player_moves_right(game);
+	// move_right_d(game);
 	// else if (key == 'a' || key == 'A')
-	// 	player_moves_left(game);
+	// 	move_left_a(game);
 	// else if (key == KEY_RIGHT_ARROW)
-	// 	player_rotates_right(game);
+	// 	rotate_right(game);
 	// else if (key == KEY_LEFT_ARROW)
-	// 	player_rotates_left(game);
+	// 	rotate_left(game);
 	if (key == KEY_ESCAPE)
 		game_exit(game, "\nThanks for playing\n\n", EXIT_SUCCESS);
 	return (0);
 }
 
-void	free_textures(t_texture *txt, t_gamedata *game)
+void	free_textures(t_texture *txt, t_img *image, t_gamedata *game)
 {
 	if (txt->img_north)
 		mlx_destroy_image(game->mlx, txt->img_north->imag);
@@ -47,13 +47,28 @@ void	free_textures(t_texture *txt, t_gamedata *game)
 		mlx_destroy_image(game->mlx, txt->img_east->imag);
 	if (txt->img_west)
 		mlx_destroy_image(game->mlx, txt->img_west->imag);
+	if (txt->img_north)
+		free(txt->img_north);
+	if (txt->img_south)
+		free(txt->img_south);
+	if (txt->img_east)
+		free(txt->img_east);
+	if (txt->img_west)
+		free(txt->img_west);
+	if (game->tex)
+		free(game->tex);
 	free(txt);
+	if (game->img->imag)
+		free(game->img->imag);
+	if (game->img)
+		free(game->img);
+	free(image);
 }
 
 void	mlx_cleanup(t_gamedata *game)
 {
 	if (game->tex)
-		free_textures(game->tex, game);
+		free_textures(game->tex, game->img, game);
 	if (game->win)
 		mlx_destroy_window(game->mlx, game->win);
 	if (game->mlx)
@@ -74,9 +89,7 @@ void	game_exit(t_gamedata *game, char *err_msg, int exit_status)
 		free(game->map_nsew[2]);
 	if (game->map_nsew[3])
 		free(game->map_nsew[3]);
-	if (game->img->imag)
-		free(game->img->imag);
-	if (game->img)
-		free(game->img);
+	if (game->mlx)
+		free(game->mlx);
 	exit(exit_status);
 }
